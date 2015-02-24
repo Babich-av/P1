2e305f9ad97bc125c29a49fd9eebd2bff72fffb3
Babich-av@ukr.net <Babich-av@ukr.net>
2015-02-25 00:16:55 +0200

    first commit

---
+++ jquery/some.js
@@ -0,0 +1,20 @@
+
+/*$(document).ready(function() {
+
+
+
+
+
+ /*var hideF = $('#formHide').toggle(function(){
+        $('#hide').fadeOut(2000);
+        $(this).text('Развернуть форму');
+    }, function(){
+        $('#hide').fadeIn(2000);
+        $(this).text('Свернуть форму');
+    })
+
+;})*/
+   /* function lesson(){
+        alert('Рады видеть Вас на нашей странице!');
+        setTimeout(lesson,3000);
+    }})*/
